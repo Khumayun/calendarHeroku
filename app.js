96f4724d1c392423e619f0cc0c06e8d4e27987c1
Khumayun <huba4buba@gmail.com>
2017-04-02 14:37:59 -0700

    dbadding

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 
-//connect to the mongoDB
-//var db = require('mongoskin').db("mongodb://localhost:27017/test", { w: 0});
-//    db.bind('event');
+connect to the mongoDB
+var db = require('mongoskin').db("mongodb://localhost:27017/test", { w: 0});
+    db.bind('event');
 
-//create express app, use public folder for static files
+create express app, use public folder for static files
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,7 +32,7 @@ app.get('/init', function(req, res){
 });
 
 
-app.get('/data', function(req, res){/*
+app.get('/data', function(req, res){
     db.event.find().toArray(function(err, data){
         //set id property for all records
         for (var i = 0; i < data.length; i++)
@@ -40,10 +40,10 @@ app.get('/data', function(req, res){/*
 
         //output response
         res.send(data);
-    });*/
+    });
 });
 
-app.post('/data', function(req, res){/*
+app.post('/data', function(req, res){
     var data = req.body;
 
     //get operation type
@@ -77,7 +77,7 @@ app.post('/data', function(req, res){/*
     else if (mode == "deleted")
         db.event.removeById( sid, update_response);
     else
-        res.send("Not supported operation");*/
+        res.send("Not supported operation");
 });
 
 app.listen(process.env.PORT || 5000);
